434be075ad7edf15ef8599c0a888de8dd13bdcbd
Larn Balaji Kesavan <LarnBalaji@users.noreply.github.com>
2014-09-23 18:36:32 +0800

    Initial Checkin

---
+++ src/main/java/numbers/MultiplyNumbers.java
@@ -0,0 +1,16 @@
+package numbers;
+
+/**
+ * Created by bkesavan on 9/23/2014.
+ */
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println("C:"+c);
+        System.out.println("--");
+
+    }
+}
